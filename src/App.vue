399f65b48ea5b43fc7e1aab43c32d4d37b416820
Pro100-Almaz <almaz.amanzholuly@nu.edu.kz>
2023-03-27 17:23:11 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header class="header">#todo</header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.header {
+  font-family: "Raleway";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 42px;
+  text-align: center;
+  letter-spacing: -0.045em;
+  padding-top: 2rem;
+  padding-bottom: 4rem;
+  color: #333333;
+}
+</style>
---
+++ src/stores/counter.ts
@@ -0,0 +1,50 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+
+export interface ToDo {
+  currentTodo: string;
+  listTodos: String[];
+  inProcessTodos: String[];
+  completedTodos: String[];
+}
+
+export const useTodoStore = defineStore("todo", {
+  state: () =>
+    ({
+      currentTodo: "",
+      listTodos: [],
+      inProcessTodos: [],
+      completedTodos: [],
+    } as ToDo),
+  actions: {
+    addTodo(todo: string) {
+      this.listTodos.push(todo);
+      this.inProcessTodos.push(todo);
+    },
+    completeTodo(todo: string) {
+      for (let i = 0; i < this.inProcessTodos.length; i++) {
+        if (this.inProcessTodos[i] == todo) {
+          this.inProcessTodos.splice(i, 1);
+          break;
+        }
+      }
+      this.completedTodos.push(todo);
+      this.listTodos.push(todo);
+    },
+    deleteTodo(todo: string) {
+      for (let i = 0; i < this.completedTodos.length; i++) {
+        if (this.completedTodos[i] == todo) {
+          this.completedTodos.splice(i, 1);
+          break;
+        }
+      }
+      for (let i = 0; i < this.listTodos.length; i++) {
+        if (this.listTodos[i] == todo) {
+          this.listTodos.splice(i, 1);
+          break;
+        }
+      }
+    },
+  },
+  getters: {},
+});
