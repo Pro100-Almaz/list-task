bc1f573672f08a5f5a82adad8fd3e4a5b55e5098
Pro100-Almaz <almaz.amanzholuly@nu.edu.kz>
2023-03-29 08:10:41 +0600

    Added: Active and Complited sections in main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,24 +2,22 @@
 import { ref, onMounted, computed, watch } from "vue";
 
 const todos = ref([]);
-const name = ref("");
+const activeTodos = ref([]);
 
 const input_content = ref("");
-const input_category = ref(null);
 
-const todos_asc = computed(() =>
+const todosAsc = computed(() =>
   todos.value.sort((a, b) => {
     return b.createdAt - a.createdAt;
   })
 );
 
 const addTodo = () => {
-  if (input_content.value.trim() === "" || input_category.value === null) {
+  if (input_content.value.trim() === "") {
     return;
   }
   todos.value.push({
     content: input_content.value,
-    category: input_category.value,
     done: false,
     createdAt: new Date().getTime(),
   });
@@ -28,6 +26,10 @@ const addTodo = () => {
 
 const removeTodo = (todo) => {
   todos.value = todos.value.filter((t) => t !== todo);
+};
+
+const removeTodoAll = () => {
+  todos.value = [];
 };
 
 watch(
@@ -40,42 +42,126 @@ watch(
   }
 );
 
-watch(name, (newVal) => {
-  localStorage.setItem("name", newVal);
-});
-
 onMounted(() => {
-  name.value = localStorage.getItem("name");
   todos.value = JSON.parse(localStorage.getItem("todos")) || [];
 });
 </script>
 
 <template>
   <TabView>
-    <TabPanel header="All"> </TabPanel>
+    <TabPanel header="All">
+      <div class="input-section">
+        <InputText
+          v-model="input_content"
+          placeholder="add details"
+          style="
+            width: 100%;
+            border: 1px solid #bdbdbd;
+            border-radius: 12px;
+            height: 56px;
+          "
+        />
+        <Button
+          label="Add"
+          style="border-radius: 12px; padding: 0 3rem"
+          @click="addTodo()"
+        />
+      </div>
+      <section class="todo-list">
+        <div class="list">
+          <div
+            v-for="todo in todosAsc"
+            :class="`todo-item ${todo.done && 'done'}`"
+          >
+            <div style="margin-right: 1rem">
+              <Checkbox v-model="todo.done" :binary="true" />
+              <!-- <span :class="`bubble ${todo.category}`"></span> -->
+            </div>
+
+            <div class="todo-content">
+              <input type="text" v-model="todo.content" />
+            </div>
+
+            <!-- <div class="actions">
+              <button class="delete" @click="removeTodo(todo)">Delete</button>
+            </div> -->
+          </div>
+        </div>
+      </section>
+    </TabPanel>
     <TabPanel header="Active">
-      <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non
-        numquam eius modi.
-      </p>
+      <div class="input-section">
+        <InputText
+          v-model="input_content"
+          placeholder="add details"
+          style="
+            width: 100%;
+            border: 1px solid #bdbdbd;
+            border-radius: 12px;
+            height: 56px;
+          "
+        />
+        <Button
+          label="Add"
+          style="border-radius: 12px; padding: 0 3rem"
+          @click="addTodo()"
+        />
+      </div>
+      <section class="todo-list">
+        <div class="list">
+          <div
+            v-for="todo in todosAsc"
+            :class="`todo-item ${todo.done && 'to-show'}`"
+          >
+            <div style="margin-right: 1rem">
+              <Checkbox v-model="todo.done" :binary="true" />
+              <!-- <span :class="`bubble ${todo.category}`"></span> -->
+            </div>
+
+            <div class="todo-content">
+              <input type="text" v-model="todo.content" />
+            </div>
+
+            <!-- <div class="actions">
+              <button class="delete" @click="removeTodo(todo)">Delete</button>
+            </div> -->
+          </div>
+        </div>
+      </section>
     </TabPanel>
     <TabPanel header="Complited">
-      <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui
-        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-        et quas molestias excepturi sint occaecati cupiditate non provident,
-        similique sunt in culpa qui officia deserunt mollitia animi, id est
-        laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita
-        distinctio. Nam libero tempore, cum soluta nobis est eligendi optio
-        cumque nihil impedit quo minus.
-      </p>
+      <section class="todo-list">
+        <div class="list">
+          <div
+            v-for="todo in todosAsc"
+            :class="`todo-item ${todo.done && 'done'}`"
+          >
+            <div style="margin-right: 1rem">
+              <Checkbox v-model="todo.done" :binary="true" />
+              <!-- <span :class="`bubble ${todo.category}`"></span> -->
+            </div>
+
+            <div class="todo-content">
+              <input type="text" v-model="todo.content" />
+            </div>
+
+            <div class="actions">
+              <button class="delete" @click="removeTodo(todo)">Delete</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="actions">
+        <button class="delete" @click="removeTodoAll">Delete all</button>
+      </div>
     </TabPanel>
   </TabView>
 </template>
 
-<style scoped></style>
+<style scoped>
+.input-section {
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+}
+</style>
